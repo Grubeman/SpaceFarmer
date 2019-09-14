55a7192d07723f3ccf5c0deaf2524098eaf9fc13
Grubeman <sebastien.tisseyre0016@orange.fr>
2019-09-14 22:04:19 +0200

    Ajout template

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from models.crop import Crop
-
-from flask import Flask
+from models.field import Field
+from models.weather import Weather
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world !"
+    field_1 = Field()
+    crop_1 = Crop("Crop1", field_1)
+    weather = Weather()
+    data = []
+    for i in range(365):
+        crop_1.grow(weather)
+        data.append({"y":crop_1.height})
+        
+    return render_template("test.html", dataset = data)
 
 if __name__ == "__main__":
     app.run()
